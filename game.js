b546ba9102f34782640a8215a668700fc21fb0b2
Victoria Kaihe <tori.ellen.g.k@gmail.com>
2018-02-24 17:02:20 +1300

    Hide dots

--- game.js
+++ game.js
@@ -13,6 +13,7 @@ function bindEventListeners (dots) {
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
     dots[i].addEventListener('click', makeBlue)
+    dots[i].addEventListener('dblclick', hide)
   }
 }
 
@@ -29,6 +30,10 @@ function makeBlue (evt) {
 }
 
 // CREATE FUNCTION hide HERE
+function hide (evt) {
+  evt.target.classList.toggle('invisible')
+  updateCounts()
+}
 
 function updateCounts () {
   var totals = {
